203e060f3da60ffd4bb46e563a4d592ee8e9bd86
semen1032 <51064226+semen1032@users.noreply.github.com>
2021-08-04 01:03:46 +0300

    Добавлен контроллер обработки пользователя

---
+++ Controllers/UserController.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using mst.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mst.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private DatabaseContext _db;
+        public UserController(DatabaseContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create([FromQuery] Referee referee)
+        {
+            if (_db.Referees.Where(x => x.Email == referee.Email).ToList().Count == 0)
+            {
+                await _db.Referees.AddAsync(referee);
+                await _db.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest("Пользователь с таким email уже существует");
+        }
+
+        [HttpPost("Edit")]
+        public async Task<IActionResult> Edit([FromQuery] Referee referee)
+        {
+            var r = _db.Referees.Find(referee.Id);
+            r.FullName = referee.FullName;
+            r.Bio = referee.Bio;
+            r.City = referee.City;
+            r.Avatar = referee.Avatar;
+            r.Email = referee.Email;
+            _db.Referees.Update(r);
+
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPost("Delete")]
+        public async Task<IActionResult> Delete([FromQuery] int referee_id)
+        {
+
+            var r = _db.Find<Referee>(referee_id);
+            if (r != null)
+            {
+                _db.Remove<Referee>(r);
+                await _db.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest("Пользователь с указанным id не найден");
+        }
+
+        [HttpGet("Get")]
+        public IActionResult Get([FromQuery] int referee_id)
+        {
+            var referee = _db.Referees.Where(referee => referee.Id == referee_id);
+            if (referee == null) {
+                return BadRequest("Пользователь не найден");
+            }
+            return Ok(referee);
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(_db.Referees);
+        }
+    }
+}
