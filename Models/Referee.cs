3bed5ea4b94a364f3a47fa5c83be748367f94e16
semen1032 <51064226+semen1032@users.noreply.github.com>
2021-08-04 00:17:58 +0300

    Обновлена модель пользователя

--- Models/Referee.cs
+++ Models/Referee.cs
@@ -6,9 +6,9 @@ namespace mst.Models {
         [Key]
         public int Id { get; set; }
         public string Email { get; set; }
-        public string FIO { get; set; }
+        public string FullName { get; set; }
         public byte[] Avatar { get; set; }
-        public string Description { get; set; }
+        public string Bio { get; set; }
         public string City { get; set; }
     }
 }
