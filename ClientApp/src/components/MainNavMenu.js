015e787b6ef51184aadf06086d8269120c239e07
ALLександр <31666804+saner99@users.noreply.github.com>
2021-08-23 15:55:21 +0300

    Update MainNavMenu.js
    добавлен пункт меню

--- ClientApp/src/components/MainNavMenu.js
+++ ClientApp/src/components/MainNavMenu.js
@@ -19,15 +19,14 @@ const [collapsed, setCollapsed] = useState(true);
         <header className="header">
         <a className="logo-header"><img src={require("../img/logo.svg")} alt=""/></a>
         <nav className="nav-header">
+          <a onClick={()=>newPage("https://musicalheart.ru/uncategorized/kratkaya-instrukcziya-po-golosovaniyu/")} className="nav-item">Порядок голосования</a>
           <a onClick={()=>newPage("http://musicalheart.ru")} className="nav-item">Главная</a>
           <a onClick={()=>newPage("https://musicalheart.ru/o-proekte/")} className="nav-item">О проекте</a>
           <a onClick={()=>newPage("https://musicalheart.ru/prezident/")} className="nav-item">Персоналии</a>
           <a onClick={()=>newPage("https://musicalheart.ru/festival-2021/")} className="nav-item">Фестиваль 2021</a>
           <a onClick={()=>newPage("https://musicalheart.ru/category/novosti/")} className="nav-item">Новости</a>
-          {/* изменены пункты меню */}
 
         <a onClick={()=>newPage("https://musicalheart.ru/o-proekte/kontakty/")} className="nav-item btn btn-primary" style={{border: 'none'}}><span>Контакты</span></a> 
-        {/* изменены текст и ссылка кнопки. надеюсь, валидно... */}
         </nav>
       </header>
     );
