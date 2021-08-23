49ca5197b982f8b20220f344200c5089deca57e2
RodionSemenov <rsemenov@cleverence.ru>
2021-08-23 13:55:38 +0300

    vimeo

--- ClientApp/src/helper/IframeHelper.js
+++ ClientApp/src/helper/IframeHelper.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export function getYouTubeUrl(url){
-    return url && url.replace("youtu.be/","www.youtube.com/watch?v=").replace('watch?v=', 'embed/').split('&')[0]+'?rel=0&modestbranding=1&showinfo=0&iv_load_policy=3'; //
+    if(url&&url.includes('youtu')){
+
+        return url.replace("youtu.be/","www.youtube.com/watch?v=").replace('watch?v=', 'embed/').split('&')[0]+'?rel=0&modestbranding=1&showinfo=0&iv_load_policy=3';
+    } 
+    else if (url&&url.includes('vimeo')){
+        return url.split('?')[0] + '?title=0&amp;byline=0&amp;portrait=0';
+    }
+    
 }
